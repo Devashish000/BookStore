21bb259f3bb223754cdd9ebb21ab6e44eaae60f2
Devashish rai <raidevashish0@gmail.com>
2024-08-17 15:50:47 +0530

    Frontend Done!!

---
+++ Frontend/src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center">
+        <h1 className="font-bold text-lg">Contact Us</h1>
+      </div>
+    </>
+  );
+}
+
+export default Contact;
---
+++ Frontend/src/contact/Contacts.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from "../components/Navbar";
+import Contact from "../components/Contact";
+
+
+
+function contact() {
+  return (
+    <div>
+      <Navbar/>
+      <Contact/>
+    </div>
+  )
+}
+
+export default contact
